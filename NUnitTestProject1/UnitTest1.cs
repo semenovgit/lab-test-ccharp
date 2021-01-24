ab98ff80f9a8577f84fa5498477d7ad835baad86
semenovgit <andrii.post@gmail.com>
2021-01-24 03:12:49 +0200

    2 commit

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -1,18 +1,29 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
 
 namespace NUnitTestProject1
 {
     public class Tests
     {
+        private IWebDriver driver;
         [SetUp]
         public void Setup()
         {
+            // перед тестами
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.cian.ru");
+            driver.Manage().Window.Maximize();
         }
 
         [Test]
         public void Test1()
         {
             Assert.Pass();
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            // после тестов
         }
     }
 }
